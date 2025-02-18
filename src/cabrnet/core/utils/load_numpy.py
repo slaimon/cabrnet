c110c12d8839a1f79dc544b2a2bb3c39a65a1076
Simone Bertolucci <84591525+slaimon@users.noreply.github.com>
2025-02-18 17:33:12 +0100

    Add load_rand to load a dataset of random data

--- src/cabrnet/core/utils/load_numpy.py
+++ src/cabrnet/core/utils/load_numpy.py
@@ -3,10 +3,18 @@ import numpy as np
 
 from torch.utils.data import TensorDataset
 
+def load_rand(in_shape: tuple[int], size: int, channels: int=3):
+    expansion_shape = (-1,channels) + len(in_shape)*(-1,)
+    samples = torch.rand((size,) + tuple(in_shape)).unsqueeze(1).expand(expansion_shape)
+    labels = torch.zeros(size).type(torch.LongTensor)
+    labels[size//2:] = 1
+    print(f"loaded dataset with samples tensor of shape: {samples.shape}")
+    return TensorDataset(samples, labels)
+
 def load_zeros(in_shape: tuple[int], size: int, channels: int=3):
     expansion_shape = (-1,channels) + len(in_shape)*(-1,)
     samples = torch.zeros((size,) + tuple(in_shape)).unsqueeze(1).expand(expansion_shape)
-    labels = torch.zeros((size,1))
+    labels = torch.zeros(size).type(torch.LongTensor)
     print(f"loaded dataset with samples tensor of shape: {samples.shape}")
     return TensorDataset(samples, labels)
 
