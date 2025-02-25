9696a22c22fb58ff7bc4e2a281ed7ea28156eaf2
Simone Bertolucci <84591525+slaimon@users.noreply.github.com>
2025-02-25 10:16:57 +0100

    Create load_torch_dataset

--- src/cabrnet/core/utils/load_numpy.py
+++ src/cabrnet/core/utils/load_numpy.py
@@ -18,65 +18,16 @@ def load_zeros(in_shape: tuple[int], size: int, channels: int=3):
     print(f"loaded dataset with samples tensor of shape: {samples.shape}")
     return TensorDataset(samples, labels)
 
-def load_numpy_dataset(path: str):
-    """
-    Load a dataset in numpy format from disk. A numpy dataset is made up of two files in binary, C-order format called
-    `"name.samples.np"` and `"name.labels.np"`.
+def load_torch_dataset(path: str):
 
-    Args:
-        path: Path to the dataset without file extension (e.g. `"path/to/dataset/name"` will read the files
-        `path/to/dataset/name.samples.np` and `path/to/dataset/name.labels.np`).
+    data = torch.load(path+".data.pt")
+    labels = torch.load(path+".labels.pt")
 
-    Returns:
-        A pytorch TensorDataset containing the same data as the numpy dataset
+    print(f"loading {path} data and labels...")
 
-    """
-
-    data_file = path + '.samples.npy'
-    labels_file = path + '.labels.npy'
-    with open(data_file, "rb") as ifp:
-        data = np.load(ifp)
-    with open(labels_file, "rb") as ifp:
-        labels = np.load(ifp)
-
-    # create dataset
-    data = torch.from_numpy(data).unsqueeze(1).expand(-1,3,-1,-1,-1)
-    labels = torch.from_numpy(labels)
+    # add the channel dimension
+    data = data.unsqueeze(1).expand(-1,3,-1,-1,-1)
 
     print(f"data: {data.shape}\nlabels: {labels.shape}")
-
-    return TensorDataset(data, labels)
-
-
-import os
-
-def load_dataset(path:str):
-    path = path + '/' if path[-1]!='/' else path
-
-    classes = []
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            classes.append(entry.name)
-
-    # load the dataset from disk
-    data = []
-    labels = []
-    for i,label in enumerate(classes):
-        print(f"loading {label} samples...")
-        for entry in os.scandir(path + label):
-            _, ext = os.path.splitext(entry.name)
-            if ext == ".npy":
-                npy_img = np.load(entry.path)
-                data.append(npy_img)
-                labels.append(i)
-
-    print(f"converting to torch tensors...")
-    data = np.array(data)
-    # add redundant channels
-    data = torch.from_numpy(data).unsqueeze(1).expand(-1,3,-1,-1,-1)
-    # transpose the shortest data dimension to dim=2
-    data = torch.transpose(data, 2, 4).type(torch.FloatTensor)
-    labels = torch.from_numpy(np.array(labels)).type(torch.LongTensor)
-    print(f"shape of data: {data.shape}")
 
     return TensorDataset(data, labels)
