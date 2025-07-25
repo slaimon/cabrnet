2907182b899aded239e986b112f7dce4e7e9fad2
Simone Bertolucci <84591525+slaimon@users.noreply.github.com>
2025-07-25 17:17:26 +0200

    Create new dataset-loading function (multithreaded)

--- src/cabrnet/core/utils/load_3d.py
+++ src/cabrnet/core/utils/load_3d.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import random
 
@@ -14,6 +16,8 @@ from torch.utils.data import TensorDataset
 
 from torchvision.datasets import Kinetics
 from torchvision import transforms as Transforms
+from tqdm import tqdm
+
 
 def load_rand(in_shape: tuple[int], size: int, channels: int=3):
     expansion_shape = (-1,channels) + len(in_shape)*(-1,)
@@ -114,6 +118,54 @@ class DefaultKineticsTransform(torch.nn.Module):
             frames[j] = self.transform(clip[j,:,:,:]) # C, H, W
         return torch.transpose(frames, 0, 1) # C, T, H, W
 
+
+
+from pytorchvideo.data.encoded_video_pyav import EncodedVideo
+from torchvision.transforms import Compose, Lambda
+from cabrnet.core.utils.utils_3d import UniformTemporalSubsample
+
+class VideoClip:
+    def __init__(self,
+             path: str,
+             start_sec: Fraction = 0,
+             duration_sec: Fraction = 1,
+             num_frames: int = 5,
+             ratio: Fraction = Fraction(10,7),
+             height: int = 180
+    ):
+        self.clip = load_video_sample(path, start_sec, duration_sec, num_frames, ratio, height)
+        self.duration = duration_sec
+        self.frames = num_frames
+        self.height = height
+        self.width = int(ratio*height)
+
+    def toTensor(self):
+        return self.clip
+
+def load_video_sample(
+        path: str,
+        start_sec: int | Fraction = 0,
+        duration_sec: int | Fraction = 1,
+        num_frames: int = 5,
+        ratio: Fraction = Fraction(10, 7),
+        height: int = 180
+):
+    video = EncodedVideo.from_path(path)
+
+    h = height
+    w = int(ratio * h)
+
+    start_sec = Fraction(start_sec)
+    end_sec = Fraction(start_sec + duration_sec)
+    video_data = video.get_clip(start_sec, end_sec)["video"] # C,T,H,W
+    transform = Compose([
+        UniformTemporalSubsample(num_frames),
+        Lambda(lambda x: x/255.0),
+        Transforms.Resize((h,w), interpolation=Transforms.InterpolationMode.NEAREST)
+    ])
+
+    return transform(video_data)
+
 class KineticsDataset(Dataset):
     def __init__(self,
                  path: str,
@@ -161,50 +213,56 @@ def load_kinetics400 (
 ):
     return KineticsDataset(path, split, subsampling, transform, clip_length, step_between_clips, ratio, height)
 
+from multiprocessing import Pool
+class Loader(object):
+    def __init__(self, **kwargs):
+        self.args = kwargs
 
-from pytorchvideo.data.encoded_video_pyav import EncodedVideo
-from torchvision.transforms import Compose, Lambda
-from cabrnet.core.utils.utils_3d import UniformTemporalSubsample
+    def __call__(self, path):
+        return load_video_sample(path,
+                                 self.args["start_sec"],
+                                 self.args["duration_sec"],
+                                 self.args["num_frames"],
+                                 self.args["ratio"],
+                                 self.args["height"])
 
-class VideoClip:
+class KineticsDatasetNew(Dataset):
     def __init__(self,
-             path: str,
-             start_sec: int | Fraction = 0,
-             duration_sec: int | Fraction = 1,
-             num_frames: int = 5,
-             ratio: int | Fraction = Fraction(10,7),
-             height: int = 180
-    ):
-        self.clip = load_video_sample(path, start_sec, duration_sec, num_frames, ratio, height)
-        self.duration = duration_sec
-        self.frames = num_frames
-        self.height = height
-        self.width = int(ratio*height)
+                 path: str,
+                 split: str,
+                 num_frames: int,
+                 duration_sec: Fraction,
+                 time_between_clips: int,
+                 ratio: Fraction,
+                 height: int
+                 ):
+        basedir = os.path.join(path,split)
+        subdirs = [ directory for directory in os.scandir(basedir) if os.path.isdir(directory.path) ]
+        self.set : list[tuple[torch.Tensor,int]] = []
 
-    def toTensor(self):
-        return self.clip
+        with Pool(3) as pool:
+            for i,subdir in enumerate(subdirs):
+                files = [ file.path for file in os.scandir(subdir) if os.path.isfile(file.path) ]
+                logger.info(f"Loading {split}:{subdir.name}...")
+                tensors = pool.map(
+                    Loader(start_sec=0, duration_sec=duration_sec, num_frames=num_frames, ratio=ratio, height=height),
+                    files
+                )
+                self.set.extend([(t,i) for t in tensors])
 
-def load_video_sample(
+    def __len__(self):
+        return len(self.set)
+
+    def __getitem__(self, item):
+        return self.set[item]
+
+def load_kinetics_new (
         path: str,
-        start_sec: int | Fraction = 0,
-        duration_sec: int | Fraction = 1,
-        num_frames: int = 5,
-        ratio: Fraction = Fraction(10, 7),
+        split: str,
+        num_frames: int,
+        duration_sec: Fraction,
+        time_between_clips: int = 5,
+        ratio: Fraction = Fraction(10,7),
         height: int = 180
 ):
-    video = EncodedVideo.from_path(path)
-
-    h = height
-    w = int(ratio * h)
-    resize = Transforms.Resize((h, w), interpolation=Transforms.InterpolationMode.NEAREST)
-
-    start_sec = Fraction(start_sec)
-    end_sec = Fraction(start_sec + duration_sec)
-    video_data = video.get_clip(start_sec, end_sec)["video"] # C,T,H,W
-    transform = Compose([
-        UniformTemporalSubsample(num_frames),
-        Lambda(lambda x: x/255.0),
-        Lambda(lambda x: torch.stack([resize(frame) for frame in [x[:,i,:,:] for i in range(x.shape[1])]], dim=1))
-    ])
-
-    return transform(video_data)
+    return KineticsDatasetNew(path, split, num_frames, duration_sec, time_between_clips, ratio, height)
